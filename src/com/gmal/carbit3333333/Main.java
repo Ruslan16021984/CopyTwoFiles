0c987fc2fe24d9c31ad029453d749b40dfa5525f
Ruslan16021984 <carbit3333333@gmail.com>
2018-10-23 09:42:42 +0300

    change metods

--- src/com/gmal/carbit3333333/Main.java
+++ src/com/gmal/carbit3333333/Main.java
@@ -8,13 +8,13 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		MyFileCopy myCopy = new MyFileCopy();
+		
 		File fileOne = new File("E:\\Работа\\Увви\\a.txt");
 		File fileTwo = new File("E:\\Работа\\Увви\\b.txt");
 		File target = new File("ab.txt");
-		String copy = myCopy.copy(fileOne, fileTwo);
-		myCopy.result(copy, target);
 
+		MyFileCopy myCopy = new MyFileCopy(fileOne, fileTwo, target);
+		myCopy.result();
 	}
 
 }
--- src/com/gmal/carbit3333333/MyFileCopy.java
+++ src/com/gmal/carbit3333333/MyFileCopy.java
@@ -6,57 +6,79 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 
 public class MyFileCopy {
+	private String words;
+	private File fileOne;
+	private File fileTwo;
+	private File targetFile;
 
-	public static String copy(File fileOne, File fileTwo) {
-		String s = "";
-		StringBuilder builder = new StringBuilder();
-		try (BufferedReader readerOne = new BufferedReader(new FileReader(fileOne));
-				BufferedReader readerTwo = new BufferedReader(new FileReader(fileTwo))){
-			String arg = "";
-
-			for (; (arg = readerOne.readLine())!=null;) {
-				builder.append(arg);
-			}
-			for (; (arg = readerTwo.readLine())!=null;) {
-				builder.append(arg);
-				
-			}
-			String[] array = builder.toString().split(" ");
-			choseWords(array);
-		} catch (IOException e) {
-			e.printStackTrace();
-			// TODO: handle exception
-		}
-		return builder.toString();
 	
-	}
-	public static void result(String words, File torget) {
-		try (PrintWriter writer = new PrintWriter("ab.txt");){
 
-			writer.write(words);
+	public MyFileCopy(File fileOne, File fileTwo, File targetFile) {
+		super();
+		this.fileOne = fileOne;
+		this.fileTwo = fileTwo;
+		this.targetFile = targetFile;
+	}
+	public void result() {
+		copy(fileOne, fileTwo);
+		
+		try (PrintWriter writer = new PrintWriter(targetFile)) {
+
+			writer.write(this.words);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	private static String choseWords(String[] array) {
-		String words= "";
+
+	private void copy(File fileOne, File fileTwo) {
+		String s = "";
+		String[] oneArray = null;
+		String[] twoArray = null;
+		StringBuilder builder = new StringBuilder();
+		try (BufferedReader readerOne = new BufferedReader(new FileReader(fileOne));
+				BufferedReader readerTwo = new BufferedReader(new FileReader(fileTwo))) {
+			String arg = "";
+
+			for (; (arg = readerOne.readLine()) != null;) {
+				oneArray = arg.split(" ");
+				System.out.println(Arrays.toString(oneArray));
+			}
+			for (; (arg = readerTwo.readLine()) != null;) {
+				twoArray = arg.split(" ");
+				System.out.println(Arrays.toString(twoArray));
+			}
+			
+			choseWords(oneArray, twoArray);
+		} catch (IOException e) {
+			e.printStackTrace();
+			// TODO: handle exception
+		}
+
+	}
+
+	private String choseWords(String[] array1, String[]array2) {
+		String words = "";
 		int count;
-		for (int i = 0; i < array.length; i++) {
+		for (int i = 0; i < array1.length; i++) {
 			count =0;
-			for (int j = 0; j < array.length; j++) {
-				if(array[i]==array[j] && count<2) {
-					words+= array[i]+" ";
-					count++;
-					
+			for (int j = 0; j < array2.length; j++) {
+				if(array1[i].equals(array2[j]) && count<=1){
+					words+=array1[i] + " ";
+					count+=1;
+					System.out.println(words);
 				}
 			}
+			
 		}
-		return words;
-		//System.out.println(words);
+
+		this.words = words;
+		return this.words;
+
 	}
-	
+
 }
